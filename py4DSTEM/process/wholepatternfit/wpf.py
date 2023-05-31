8da9c7a362d7c9dddb09592c9b1bd6f38c3bf596
alex-rakowski <alexrakowski90@gmail.com>
2023-05-31 01:40:04 -0700

    adding dask_results

--- py4DSTEM/process/wholepatternfit/wpf.py
+++ py4DSTEM/process/wholepatternfit/wpf.py
@@ -504,6 +504,8 @@ class WholePatternFit:
         jobs = partition_all(100, jobs)
         # do the computation 
         results = client.compute(jobs, optimize_graph=True)
+
+        self._dask_results = results
         # progress(results, notebook=True) # this isn't working 
         # gather the results
         results = client.gather(results)
