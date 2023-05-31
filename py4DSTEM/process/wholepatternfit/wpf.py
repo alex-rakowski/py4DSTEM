f8a722b22526103548bc4ada56019498b8242513
alex-rakowski <alexrakowski90@gmail.com>
2023-05-31 01:39:25 -0700

    optimize_graph check

--- py4DSTEM/process/wholepatternfit/wpf.py
+++ py4DSTEM/process/wholepatternfit/wpf.py
@@ -503,7 +503,7 @@ class WholePatternFit:
 
         jobs = partition_all(100, jobs)
         # do the computation 
-        results = client.compute(jobs, optimize_graph=False)
+        results = client.compute(jobs, optimize_graph=True)
         # progress(results, notebook=True) # this isn't working 
         # gather the results
         results = client.gather(results)
